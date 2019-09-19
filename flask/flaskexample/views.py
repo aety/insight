ee1d0cd47b541ee6205f33169e7fce2223b92be0
aety <anneyanget@gmail.com>
2019-09-19 17:46:27 -0500

    got the /db page to query from auber_pm10_db  with crappy looks

--- flask/flaskexample/views.py
+++ flask/flaskexample/views.py
@@ -4,19 +4,17 @@ from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
 import pandas as pd
 import psycopg2
-from flask import request
-from a_Model import ModelIt
 
 username = 'postgres'
 password = 'yanggnay'     # change this
 host     = 'localhost'
 port     = '5432'            # default port that postgres listens on
-db_name  = 'birth_db'
+#db_name = 'birth_db'
+db_name = 'auber_pm10_db'
 
 db = create_engine( 'postgresql://{}:{}@{}:{}/{}'.format(username, password, host, port, db_name) )
-
 con = None
-con = psycopg2.connect(database = 'birth_db', user = 'postgres', password = 'yanggnay', port = '5432', host = 'localhost')
+con = psycopg2.connect(database = db_name, user = 'postgres', password = 'yanggnay', port = '5432', host = 'localhost')
 
 @app.route('/')
 @app.route('/index')
@@ -27,139 +25,15 @@ def index():
 
 @app.route('/db')
 def birth_page():
-    sql_query = """                                                             
-                SELECT * FROM birth_data_table WHERE delivery_method='Cesarean'\
-;                                                                               
+    sql_query = """                                                                       
+                SELECT * FROM auber_pm10_table WHERE ds>'2019-12-30 00:00:00';
                 """
+    #sql_query = """                                                                       
+    #            SELECT * FROM birth_data_table WHERE delivery_method='Cesarean';          
+    #            """
     query_results = pd.read_sql_query(sql_query,con)
     births = ""
-    print(query_results[:10])
     for i in range(0,10):
-        births += query_results.iloc[i]['testtest']
+        births += str(query_results.iloc[i]['trend'])
         births += "<br>"
     return births
-
-@app.route('/db_fancy')
-def cesareans_page_fancy():
-    sql_query = """
-               SELECT index, attendant, birth_month FROM birth_data_table WHERE delivery_method='Cesarean';
-                """
-    query_results=pd.read_sql_query(sql_query,con)
-    births = []
-    for i in range(0,query_results.shape[0]):
-        births.append(dict(index=query_results.iloc[i]['index'], attendant=query_results.iloc[i]['attendant'], birth_month=query_results.iloc[i]['birth_month']))
-    return render_template('cesareans.html',births=births)
-
-
-
-
-@app.route('/input')
-def cesareans_input():
-    return render_template("input.html")
-
-#@app.route('/output')
-#def cesareans_output():
-#    return render_template("output.html")
-
-
-
-
-@app.route('/output')
-def cesareans_output():
-  #pull 'birth_month' from input field and store it
-  patient = request.args.get('birth_month')
-    #just select the Cesareans  from the birth dtabase for the month that the user inputs
-  query = "SELECT index, attendant, birth_month FROM birth_data_table WHERE delivery_method='Cesarean' AND birth_month='%s'" % patient
-  print(query)
-  query_results=pd.read_sql_query(query,con)
-  print(query_results)
-  births = []
-  for i in range(0,query_results.shape[0]):
-      births.append(dict(index=query_results.iloc[i]['index'], attendant=query_results.iloc[i]['attendant'], birth_month=query_results.iloc[i]['birth_month']))
-#      the_result = ''
-#  return render_template("output.html", births = births, the_result = the_result)
-      the_result = ModelIt(patient,births)
-  return render_template("output.html", births = births, the_result = the_result)
-
-
-
-#from flask import render_template
-#from flaskexample import app
-#from sqlalchemy import create_engine
-#from sqlalchemy_utils import database_exists, create_database
-#import pandas as pd
-#import psycopg2
-#
-##user = 'postgres' #add your username here (same as previous postgreSQL)                      
-##host = 'localhost'
-##dbname = 'birth_db'
-#username = 'postgres'
-#password = 'yanggnay'     # change this
-#host     = 'localhost'
-#port     = '5432'            # default port that postgres listens on
-#db_name  = 'birth_db'
-#
-##db = create_engine('postgres://%s%s/%s'%(user,host,dbname))
-#db = create_engine( 'postgresql://{}:{}@{}:{}/{}'.format(username, password, host, port, db_name) )
-#
-#con = None
-##con = psycopg2.connect(database = dbname, user = user)
-#con = psycopg2.connect(database = 'birth_db', user = 'postgres', password = 'yanggnay', port = '5432', host = 'localhost')
-#
-#@app.route('/')
-#@app.route('/index')
-#def index():
-#    return render_template("index.html",
-#       title = 'Home', user = { 'nickname': 'Miguel' },
-#       )
-#
-#@app.route('/db')
-#def birth_page():
-#    sql_query = """                                                                       
-#                SELECT * FROM birth_data_table WHERE delivery_method='Cesarean';          
-#                """
-#    query_results = pd.read_sql_query(sql_query,con)
-#    births = ""
-#    for i in range(0,10):
-#        births += query_results.iloc[i]['birth_month']
-#        births += "<br>"
-#    return births
-
-#from flask import render_template
-#from flaskexample import app
-
-#@app.route('/')
-#@app.route('/index')
-#def index():
-#   user = { 'nickname': 'Miguel' } # fake user
-#   return render_template("index.html",
-#       title = 'Home',
-#       user = user)
-
-
-
-#from flaskexample import app
-
-#@app.route('/')
-#@app.route('/index')
-#def index():
-#   return "Hello, World!"
-
-
-
-#from flaskexample import app
-
-#@app.route('/')
-#@app.route('/index')
-#def index():
-#   user = { 'nickname': 'Miguel' } # fake user
-#   return '''
-#            <html>
-#             <head>
-#               <title>Home Page</title>
-#             </head>
-#             <body>
-#               <h1>Hello, ''' + user['nickname'] + '''</h1>
-#             </body>
-#            </html>
-#          '''
