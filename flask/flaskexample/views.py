d9e6398a452e5d5b46d631f71b63c417ff8e1856
aety <anneyanget@gmail.com>
2019-09-22 21:32:42 -0500

    reverted back to working tutorial example

--- flask/flaskexample/views.py
+++ flask/flaskexample/views.py
@@ -1,3 +1,6 @@
+from a_Model import ModelIt
+
+from flask import request
 from flask import render_template
 from flaskexample import app
 from sqlalchemy import create_engine
@@ -9,12 +12,12 @@ username = 'postgres'
 password = 'yanggnay'     # change this
 host     = 'localhost'
 port     = '5432'            # default port that postgres listens on
-#db_name = 'birth_db'
-db_name = 'auber_pm10_db'
+db_name = 'birth_db'
 
 db = create_engine( 'postgresql://{}:{}@{}:{}/{}'.format(username, password, host, port, db_name) )
 con = None
 con = psycopg2.connect(database = db_name, user = 'postgres', password = 'yanggnay', port = '5432', host = 'localhost')
+
 
 @app.route('/')
 @app.route('/index')
@@ -26,7 +29,8 @@ def index():
 @app.route('/db')
 def birth_page():
     sql_query = """                                                             
-                SELECT * FROM auber_pm10_table WHERE ds>'2019-12-30 00:00:00';                                                                              
+                SELECT * FROM birth_data_table WHERE delivery_method='Cesarean'\
+;                                                                               
                 """
     query_results = pd.read_sql_query(sql_query,con)
     births = ""
@@ -39,12 +43,12 @@ def birth_page():
 @app.route('/db_fancy')
 def cesareans_page_fancy():
     sql_query = """
-               SELECT * FROM auber_pm10_table WHERE ds>'2019-12-30 00:00:00';                                                   
+               SELECT index, attendant, birth_month FROM birth_data_table WHERE delivery_method='Cesarean';
                 """
     query_results=pd.read_sql_query(sql_query,con)
     births = []
     for i in range(0,query_results.shape[0]):
-        births.append(dict(index=query_results.iloc[i]['index'], attendant=query_results.iloc[i]['ds'], birth_month=str(query_results.iloc[i]['trend'])))
+        births.append(dict(index=query_results.iloc[i]['index'], attendant=query_results.iloc[i]['attendant'], birth_month=query_results.iloc[i]['birth_month']))
     return render_template('cesareans.html',births=births)
 
 
@@ -52,13 +56,25 @@ def cesareans_page_fancy():
 
 
 
-
-
-
-#@app.route('/input')
-#def cesareans_input():
-#    return render_template("input.html")
+@app.route('/input')
+def cesareans_input():
+    return render_template("input.html")
 
 #@app.route('/output')
 #def cesareans_output():
 #    return render_template("output.html")
+
+@app.route('/output')
+def cesareans_output():
+  #pull 'birth_month' from input field and store it
+  patient = request.args.get('birth_month')
+    #just select the Cesareans  from the birth dtabase for the month that the user inputs
+  query = "SELECT index, attendant, birth_month FROM birth_data_table WHERE delivery_method='Cesarean' AND birth_month='%s'" % patient
+  print(query)
+  query_results=pd.read_sql_query(query,con)
+  print(query_results)
+  births = []
+  for i in range(0,query_results.shape[0]):
+      births.append(dict(index=query_results.iloc[i]['index'], attendant=query_results.iloc[i]['attendant'], birth_month=query_results.iloc[i]['birth_month']))
+  the_result = ModelIt(patient,births)
+  return render_template("output.html", births = births, the_result = the_result)
